daafc798b4061547d245b7864e2b08cb2e6e8015
Ashley aka Pooja <67260934+poojadubey01@users.noreply.github.com>
2024-10-02 20:35:27 +0530

    Add files via upload

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,21 +1,70 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import hero from '../assets/hero.jpg';  
 
 function Hero() {
+  const canvasRef = useRef(null);
+
+ 
+  const createNoise = () => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    const imageData = ctx.createImageData(canvas.width, canvas.height);
+    const data = imageData.data;
+
+   
+    for (let i = 0; i < data.length; i += 4) {
+      const noiseValue = Math.random() * 255;
+      data[i] = noiseValue; 
+      data[i + 1] = noiseValue; 
+      data[i + 2] = noiseValue; 
+      data[i + 3] = Math.random() * 350; 
+    }
+
+    ctx.putImageData(imageData, 0, 0);
+  };
+
+  
+  useEffect(() => {
+    createNoise();
+    const interval = setInterval(createNoise, 100); 
+    return () => clearInterval(interval); // 
+  }, []);
+
   return (
-    <section className="relative h-screen bg-gray text-black flex flex-col items-center justify-center opacity-0 animate-fadeIn bg-black">
-      <h1 className="text-5xl font-bold mb-6">The portable companion to the Linear system</h1>
-      <p className="text-lg mb-8">Complex workflows in compact form. Available for iOS and Android.</p>
+    <section className="relative h-screen bg-black flex flex-col items-center justify-center opacity-90 animate-fadeIn">
+     
+      <div className="relative w-full h-full">
+        <img
+          src={hero}
+          alt="Phone"
+          className="absolute top-0 left-0 w-full h-30 object-cover z-0"
+        />
+         <div className="absolute bottom-0 left-0 w-full h-1/6 bg-gradient-to-t from-black via-transparent to-transparent z-10"></div>
+       
+        <canvas
+          ref={canvasRef}
+          className="absolute top-0 left-0 w-90% h-full z-10 pointer-events-none opacity-100 mix-blend-overlay" 
+        />
+      </div>
       
-      
-      <img 
-        src={hero} 
-        alt="Phone" 
-        className="max-w-xl-50% mx-auto h-30 bg-blend-multiply mt-40"  
-      />
+      <div className="absolute bottom-0 left-0 w-full h-1/3 bg-black z-10"></div>
+       
+
+     
+      <div className="relative z-20 text-bottom text-white">
+        <h1 className="text-4xl font-bold mb-6 left-0">
+          The portable companion <br/>
+          to the Linear system
+        </h1>
+        <p className="text-lg text-gray mb-8">
+          Complex workflows in compact form.<br/> Available for iOS and Android.
+        </p>
+      </div>
     </section>
   );
 }
 
 export default Hero;
-
